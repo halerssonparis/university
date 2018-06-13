c3c6f78c6d5360bc0f82d92aaff9f2c9d71af786
halersson <halerssonparis@hotmail.com>
2018-06-13 17:56:37 -0300

    feito boa parte

--- Complexidade de Algoritimos/Busca Local/program.py
+++ Complexidade de Algoritimos/Busca Local/program.py
@@ -1,16 +1,32 @@
 import csv
 import collections
 
+class Soluctions:
+    
+    inputsSoluctions = []
+    
+    compare = lambda list1, list2: collenctions.Counter(list1) == collenctions.Counter(list2)    
 
-class bagProblem:
+    #def __init__(self):
+
+    def add(self, inputs, totalValue):
+        if (verifySoluctions(inputs)):
+            inputsSoluctions.append([inputs, totalValue])
+
+    def verifySoluctions(self, newSoluction):
+        for soluction in self.inputsSoluctions:
+            if (compare(soluction[0], newSoluction)):
+                return False
+                 
+        return True
+
+class BagProblem:
     
     inputsLength = 0
     maxCapacity = 0
     weights = []
     values = []
-    soluctions = []    
-
-    compare = lambda list1, list2: collenctions.Counter(list1) == collenctions.Counter(list2)
+    soluctions = Soluctions()
 
     def __init__(self):
         print "[*] - Welcome!"
@@ -23,19 +39,49 @@ class bagProblem:
                 if (index == 0):
                     self.inputsLength = item[0]
                     self.maxCapacity = item[1]
-                else:
-                    
-                    
-                    print item          
-        
-    def verifySoluctions(self, newSoluction):
-        for soluction in self.soluctions:
-            if (compare(soluction, newSoluction)):
-                return False
-                 
-        return True
+                elif (index == 1):
+                    self.weights = item
+                elif (index == 2):
+                    self.values = item                    
 
+    
 
-newBag = bagProblem()
-newBag.loadData("instancias2/a100.lia")
+    def searchSoluctions(self):
+        #[0] weigth  |  [1] value
+        newSoluction.append([0, 0])           
+        newWeigth = 0
+
+        for (i = 0; i < self.inputsLength; i++):
+            del newSoluction[:]
+            
+            if (values[i] < self.maxCapacity):      
+
+                newSoluction.append([weights[i], values[i])         
+                newWeigth = values[i]
+
+                for (j = i+1; j < self.inputsLength; j++):
+                    if (newWeigth + values[j] < self.maxCapacity):
+                        for (k = j; k < self.inputsLength; k++):
+                            
+                            
+
+                    elif (newWeigth + values[j] == self.maxCapacity):
+                        newSoluction.append([weights[j], values[i])
+                        newWeigth = newWeigth + values[i]
+                        self.soluctions.add(newSoluction, newWeigth)
+                        newSoluction.pop()
+                    
+            elif (values[i] == maxCapacity):
+                self.soluctions.add(weights[i], values[i])
+
+    def printValues(self):
+        print "[-] - Items:"
+        print self.inputsLength
+        print self.maxCapacity
+        print self.weights
+        print self.values
+
+BagzaoBrabo = BagProblem()
+BagzaoBrabo.loadData("instancias2/a100.lia")
+BagzaoBrabo.printValues()
 
